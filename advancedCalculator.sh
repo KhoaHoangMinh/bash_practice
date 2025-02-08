6ec41c7ffa6373585f6d529340510e70c179fe48
khoahoang <hoangkhoa2015@gmail.com>
2025-02-08 21:57:46 +0700

    Update ANS

--- advancedCalculator.sh
+++ advancedCalculator.sh
@@ -2,12 +2,22 @@
 
 calculate() {	
 	while true; do
-		ANS=0
-
+	
 		read -p ">> " num1 op num2
 		if [ $num1 == "EXIT" ]; then
 			break
 		fi
+		if [[ -f ~/.ans ]]; then
+    			ANS=$(cat ~/.ans)
+		else
+    			ANS=0
+		fi
+		if [[ "$num1" == "ANS" ]]; then
+			num1=$ANS
+		fi
+		if [[ "$num2" == "ANS" ]]; then
+			num2=$ANS
+		fi	
 	 	case $op in
 		       	+)
 			ANS=$(echo "scale=2; $num1 + $num2" | bc)
@@ -42,9 +52,10 @@ calculate() {
 			break
 			;;
 		esac
+		echo "$ANS" > ~/.ans
 		echo "$ANS"
-		read -n 1 -s
-		# -n 1: read 1 character, -s: silent mode
+		read -n 0 -s
+		# -n 0: read 1 character, -s: silent mode
 		clear
 	done	
 }

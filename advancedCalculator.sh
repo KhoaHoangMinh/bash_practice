ecdd65decf82ed7b1955e3ae392949ed9fd12611
khoahoang <hoangkhoa2015@gmail.com>
2025-02-09 15:34:18 +0700

    Update HIST

--- advancedCalculator.sh
+++ advancedCalculator.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
 calculate() {	
+	HISTORY_FILE=~/.calc_history
+	ANS_FILE=~/.ans
+
 	while true; do
-	
 		read -p ">> " num1 op num2
-		if [ $num1 == "EXIT" ]; then
+
+		if [[ $num1 == "EXIT" ]]; then
 			break
 		fi
-		if [[ -f ~/.ans ]]; then
-    			ANS=$(cat ~/.ans)
+
+		if [[ "$num1" == "HIST" ]]; then
+			if [[ -f $HISTORY_FILE ]]; then
+				cat $HISTORY_FILE
+			else
+				echo "No history"
+			fi
+			continue
+		fi	
+
+		# Load previous ANS value
+		if [[ -f "$ANS_FILE" ]]; then
+    			ANS=$(cat "$ANS_FILE")
 		else
     			ANS=0
 		fi
+
 		if [[ "$num1" == "ANS" ]]; then
 			num1=$ANS
 		fi
 		if [[ "$num2" == "ANS" ]]; then
 			num2=$ANS
 		fi	
+
 	 	case $op in
 		       	+)
 			ANS=$(echo "scale=2; $num1 + $num2" | bc)
@@ -52,9 +68,16 @@ calculate() {
 			break
 			;;
 		esac
-		echo "$ANS" > ~/.ans
+		
+		# Save answer
+		echo "$ANS" > "$ANS_FILE"
+
+		# Save history
+		echo "$num1 $op $num2 = $ANS" >> $HISTORY_FILE
+		tail -n 5 $HISTORY_FILE > $HISTORY_FILE.tmp && mv $HISTORY_FILE.tmp $HISTORY_FILE
+
 		echo "$ANS"
-		read -n 0 -s
+		read -n 1 -s -r
 		# -n 0: read 1 character, -s: silent mode
 		clear
 	done	
